0c1a6d0357940c76f7e9d579f01044b657f366eb
xingzheng <xingzheng@hotmail.com>
2021-08-09 11:21:10 +0800

    remove unused code

--- WinUI3Samples/ListViewSample/ListViewSample/Person.cs
+++ WinUI3Samples/ListViewSample/ListViewSample/Person.cs
@@ -10,27 +10,4 @@ namespace ListViewSample
     {
         public string Name { get; set; }
     }
-
-    class People
-    {
-        public List<Person> PersonList { get; set; }
-
-        public People()
-        {
-            this.PersonList = new List<Person>
-            {
-                new Person{ Name = "Abe"},
-                new Person{ Name = "Alice"},
-                new Person{ Name = "Bell"},
-                new Person{ Name = "Ben"},
-                new Person{ Name = "Bob"},
-                new Person{ Name = "Fox"},
-                new Person{ Name = "Gray"},
-                new Person{ Name = "James"},
-                new Person{ Name = "Jane"},
-                new Person{ Name = "Roy"},
-                new Person{ Name = "Vincent"}
-            };
-        }
-    }
 }
